2c120a6e8ce4573df6faab32b56bf4a40c9fc441
ИСП-31 <dunaevam2005@mail.ru>
2022-10-25 16:23:17 +0300

    Добавьте файлы проекта.

---
+++ PracticalWork7/Liquid.cs
@@ -0,0 +1,258 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Пять
+{
+    class Liquid
+    {
+        String name;
+        double density;
+        double volume;
+
+        public String Name
+        {
+            get
+            {
+                return name;
+            }
+            set
+            {
+                name = value;
+            }
+        }
+
+        public double Density
+        {
+            get
+            {
+                return density;
+            }
+            set
+            {
+                density = value;
+            }
+        }
+
+        public Double Volume
+        {
+            get
+            {
+                return volume;
+            }
+            set
+            {
+                volume = value;
+            }
+        }
+
+        public Liquid()
+        {
+            Name = "";
+            Density = 0;
+            Volume = 0;
+        }
+
+        public Liquid(string name, double density, double volume)
+        {
+            Name = name;
+            Density = density;
+            Volume = volume;
+        }
+
+        /// <summary>
+        /// Находит разницу объёмов жидкостей
+        /// </summary>
+        /// <param name="liquid">Вторая жидкость</param>
+        /// <returns>Разницу объёмов</returns>
+        public double VolumeDifference(Liquid liquid)
+        {
+            return Math.Abs(liquid.Volume - Volume);
+        }
+
+        /// <summary>
+        /// Находит разницу плотностей жидкостей
+        /// </summary>
+        /// <param name="liquid">Вторая жидкость</param>
+        /// <returns>Разницу жидкостей</returns>
+        public double DensityDifference(Liquid liquid)
+        {
+            return Math.Abs(liquid.Density - Density);
+        }
+
+        /// <summary>
+        /// Изменить назнание, плотность и объём жидкости 
+        /// </summary>
+        public void SetParams()
+        {
+            Name = "";
+            Density = 0;
+            Volume = 0;
+        }
+
+        /// <summary>
+        /// Изменить назнание, плотность и объём жидкости 
+        /// </summary>
+        public void SetParams(string name, double density, double volume)
+        {
+            Name = name;
+            Density = density;
+            Volume = volume;
+        }
+
+        /// <summary>
+        /// Возвращает информацию о жидкости, собранную в одну строку
+        /// </summary>
+        /// <returns>Информация о жидкости</returns>
+        public string LiquidInfo()
+        {
+            return "Название: " + Name + "\n" + "Плотность: " + Density.ToString() + "\n" + "Объём: " + Volume.ToString();
+        }
+
+        /// <summary>
+        /// Проверка, что сосуды имеют одинаковые жидкости равного объема.
+        /// </summary>
+        /// <param name="liquid1">Жидкость 1</param>
+        /// <param name="liquid2">Жидкость 2</param>
+        /// <returns>true, если жидкости одинаковые; false, если жидкости разные</returns>
+        public static bool operator ==(Liquid liquid1, Liquid liquid2)
+        {
+            if (liquid1.Name == liquid2.Name && liquid1.Density == liquid2.Density && liquid1.Volume == liquid2.Volume) return true;
+            else return false;
+        }
+
+        /// <summary>
+        /// Проверка, что сосуды имеют разные жидкости.
+        /// </summary>
+        /// <param name="liquid1">Жидкость 1</param>
+        /// <param name="liquid2">Жидкость 2</param>
+        /// <returns>true, если жидкости разные; false, если жидкости одинаковые</returns>
+        public static bool operator !=(Liquid liquid1, Liquid liquid2)
+        {
+            if (liquid1.Name != liquid2.Name || liquid1.Density != liquid2.Density || liquid1.Volume != liquid2.Volume) return true;
+            else return false;
+        }
+
+        /// <summary>
+        /// Увеличивает объём на 1
+        /// </summary>
+        public static Liquid operator ++(Liquid liquid)
+        {
+            liquid.Volume += 1;
+            return liquid;
+        }
+
+        /// <summary>
+        /// Уменьшает объём на 1
+        /// </summary>
+        public static Liquid operator --(Liquid liquid)
+        {
+            if (liquid.Volume > 0) liquid.Volume -= 1;
+            return liquid;
+        }
+    }
+
+    class Alcohol:Liquid
+    {
+        double strength;
+
+        public double Strength
+        {
+            get
+            {
+                return strength;
+            }
+            set
+            {
+                strength = value;
+            }
+        }
+
+        public Alcohol()
+        {
+            Name = "";
+            Density = 0;
+            Volume = 0;
+            Strength = 0;
+        }
+
+        public Alcohol(string name, double density, double volume, double strength):
+            base(name, density, volume)
+        {
+            Strength = strength;
+        }
+
+        public new void SetParams()
+        {
+            Name = "";
+            Density = 0;
+            Volume = 0;
+            Strength = 0;
+        }
+
+        public void SetParams(string name, double density, double volume, double strength)
+        {
+            Name = name;
+            Density = density;
+            Volume = volume;
+            Strength = strength;
+        }
+
+        public new string LiquidInfo()
+        {
+            return "Название: " + Name + "\n" + "Плотность: " + Density.ToString() + "\n" + 
+                "Объём: " + Volume.ToString() + "\n" + "Крепость: " + Strength.ToString();
+        }
+    }
+
+    class Beer:Alcohol
+    {
+        double hopContent;
+
+        public double HopContent
+        {
+            get
+            {
+                return HopContent;
+            }
+            set
+            {
+                hopContent = value;
+            }
+        }
+
+        public Beer(string name, double density, double volume, double strength, double hopContent) :
+            base(name, density, volume, strength)
+        {
+            HopContent = hopContent;
+        }
+
+        public new void SetParams()
+        {
+            Name = "";
+            Density = 0;
+            Volume = 0;
+            Strength = 0;
+            HopContent = 0;
+        }
+
+        public void SetParams(string name, double density, double volume, double strength, double hopContent)
+        {
+            Name = name;
+            Density = density;
+            Volume = volume;
+            Strength = strength;
+            HopContent = hopContent;
+        }
+
+        public void SetParams(double strength, double hopContent)
+        {
+            Strength = strength;
+            HopContent = hopContent;
+        }
+
+    }
+}
---
+++ PracticalWork7/MainWindow.xaml.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Пять
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        Liquid liquid1;
+        private void btn_Add_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                liquid1 = new Liquid(Convert.ToString(name.Text), Convert.ToDouble(dens.Text), Convert.ToDouble(vol.Text));
+                liquid1_info.Text = liquid1.LiquidInfo();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void btn_Dif_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                if (liquid1 != null && liquid2 != null)
+                {
+                    if (rb_dens.IsChecked == true) result.Text = Convert.ToString(liquid1.DensityDifference(liquid2));
+                    else result.Text = Convert.ToString(liquid1.VolumeDifference(liquid2));
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        Liquid liquid2;
+        private void btn_Add2_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                liquid2 = new Liquid(Convert.ToString(l2_name.Text), Convert.ToDouble(l2_dens.Text), Convert.ToDouble(l2_vol.Text));
+                liquid2_info.Text = liquid2.LiquidInfo();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void btn_Edit2_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                liquid2.SetParams(Convert.ToString(l2_name.Text), Convert.ToDouble(l2_dens.Text), Convert.ToDouble(l2_vol.Text));
+                liquid2_info.Text = liquid2.LiquidInfo();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void btn_Edit_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                liquid1.SetParams(Convert.ToString(name.Text), Convert.ToDouble(dens.Text), Convert.ToDouble(vol.Text));
+                liquid1_info.Text = liquid1.LiquidInfo();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void exit_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+
+        private void about_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Практическая работа №7\n\nДунаева М.И\n\nВариант 14\n\n" +
+                " Использовать класс Liquid (жидкость), имеющий поля названия, плотности и " +
+                "объема.Создать необходимые методы и свойства.Создать перегруженные методы" +
+                "SetParams, для установки параметров жидкости.Создать производный класс" +
+                "Alcohol(спирт), имеющий крепость.Из класса Alcohol создать производный класс" +
+                "Beer(пиво), имеющий процент содержания хмеля.Определить методы " +
+                "переназначения и изменения крепости и процента хмеля.");
+        }
+
+        private void btn_decr_Click(object sender, RoutedEventArgs e)
+        {
+            liquid1--;
+            liquid1_info.Text = liquid1.LiquidInfo();
+        }
+
+        private void btn_inc_Click(object sender, RoutedEventArgs e)
+        {
+            liquid1++;
+            liquid1_info.Text = liquid1.LiquidInfo();
+        }
+
+        private void btn_inc2_Click(object sender, RoutedEventArgs e)
+        {
+            liquid2++;
+            liquid2_info.Text = liquid2.LiquidInfo();
+        }
+
+        private void btn_decr2_Click(object sender, RoutedEventArgs e)
+        {
+            liquid2--;
+            liquid2_info.Text = liquid2.LiquidInfo();
+        }
+
+        private void btn_Compare_Click(object sender, RoutedEventArgs e)
+        {
+            if (liquid1 == liquid2) MessageBox.Show("Жидкости имеют равный объём, равную плотность и одинаковое название");
+            else MessageBox.Show("Жидкости различаются");
+        }
+    }
+}
